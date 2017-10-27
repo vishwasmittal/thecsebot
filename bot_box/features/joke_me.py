692662fdeb3093fc90ebdaf1025af981004c2174
Faizal Hasanwala <faizalhasanwala@gmail.com>
2017-10-28 02:06:09 +0530

    Optimized joke_me by removing json

--- bot_box/features/joke_me.py
+++ bot_box/features/joke_me.py
@@ -1,4 +1,4 @@
-from bot_box.features import requests, json, os
+from bot_box.features import requests, os
 
 
 def joke_me_api():
@@ -9,8 +9,8 @@ def joke_me_api():
     while True: # retries if there is any error
         try:
             url = os.environ.get('JOKE_URL')
-            joke_resp_raw = requests.get(url).text
-            joke_resp = json.loads(joke_resp_raw)
+            joke_resp_raw = requests.get(url).json()
+            # joke_resp = json.loads(joke_resp_raw)
             if joke_resp and 'attachments' in joke_resp:
                 joke = joke_resp['attachments'][0]['text']
                 return joke
