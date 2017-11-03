47738b05612898657ed28590c96b004e06716186
Vishwas Mittal <vishwas.mittal7@gmail.com>
2017-11-03 05:49:04 +0530

    code refacted and chatbot source changed

--- bot_box/features/chat_me.py
+++ bot_box/features/chat_me.py
@@ -1,7 +1,7 @@
 from bot_box.features import json, bsp, request, parse, os
 from bot_box.bot_constants import *
 
-CHATBOT_URL = os.environ.get('CHATBOT_URL')
+# CHATBOT_URL = os.environ.get('CHATBOT_URL')
 CHATBOT_BOTCUST2 = os.environ.get("CHATBOT_BOTCUST2")
 
 
@@ -13,7 +13,7 @@ def ONLINE_CHATBOT_RESP(user_input):
         :param user_input: the input of user to be fed to chat bot
         :return: the response given by chat bot
     """
-    url = CHATBOT_URL
+    url = 'https://www.pandorabots.com/pandora/talk?botid=a847934aae3456cb'
     botcust2 = CHATBOT_BOTCUST2
     headers = {"Content-Type": "application/x-www-form-urlencoded", 'user-agent': USER_AGENT}
     req = request.Request(url, headers=headers)
@@ -25,8 +25,8 @@ def ONLINE_CHATBOT_RESP(user_input):
     alice_resp = resp.read().decode()
     soup = bsp(alice_resp, 'html.parser')
     lines = soup.text.split('\n')
-    respond = lines[19]
-    if lines[20] is not "": respond += lines[20]
+    respond = lines[10]
+    if lines[10] is not "": respond += lines[11]
     respond = respond.replace(ONLINE_CHATBOT_NAME2 + ": ", "")
     respond = respond.split("   ")[0]
     respond = respond.replace(CHATBOT_CREATER, BOT_CREATER)
