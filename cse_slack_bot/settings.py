0f6d8aee68fd6d7ba35f0790f77db57b09ce03c5
vishwas78 <vishwas.mittal78@gmail.com>
2017-12-06 12:49:20 +0530

    changed allowed host

--- cse_slack_bot/settings.py
+++ cse_slack_bot/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'thecsebot.herokuapp.com']
 
 # Application definition
 
